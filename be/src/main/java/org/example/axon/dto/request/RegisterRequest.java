22e2bf903265fecebac764bfe82e9ca14e67cedb
trung0102 <trung.nguyen@hcmut.edu.vn>
2025-11-08 10:16:22 +0700

    Add exception passwords

--- be/src/main/java/org/example/axon/dto/request/RegisterRequest.java
+++ be/src/main/java/org/example/axon/dto/request/RegisterRequest.java
@@ -7,5 +7,8 @@ public record RegisterRequest(
         @NotBlank @Size(min = 8) String password,
         @NotBlank String firstName,
         @NotBlank String lastName,
-        @NotBlank String phoneNumber
+        @NotBlank
+        @Size(min = 10, max = 10, message = "Phone number must have 10 digits")
+        @Pattern(regexp = "^0[0-9]*$", message = "Phone number must start with 0")
+        String phoneNumber
 ) {}
