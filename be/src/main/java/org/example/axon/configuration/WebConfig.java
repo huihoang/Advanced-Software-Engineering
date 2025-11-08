d17c4fabbb8a93516aaed8a03c65236ff0cf0511
quechivvt <166884387+chinguyen2407k22@users.noreply.github.com>
2025-11-08 09:47:17 +0700

    Update WebConfig.java

--- be/src/main/java/org/example/axon/configuration/WebConfig.java
+++ be/src/main/java/org/example/axon/configuration/WebConfig.java
@@ -1,4 +1,4 @@
-package org.example.axon;
+package org.example.axon.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
